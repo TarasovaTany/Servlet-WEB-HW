86798570b50669402012044b01d7f79ec8322315
Tatyana Tarasova <umnova.panther@yandex.ru>
2024-08-19 10:42:18 +0500

    Changing files for Homework 2.1

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -2,24 +2,37 @@ package repository;
 
 import model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 // Stub
 public class PostRepository {
+    private final ConcurrentMap<Long, Post> allPosts;
+    private long nextId = 1;
+
+    public PostRepository() {
+        this.allPosts = new ConcurrentHashMap<>();
+    }
     public List<Post> all() {
-        return Collections.emptyList();
+        return new ArrayList<>(allPosts.values());
     }
 
     public Optional<Post> getById(long id) {
-        return Optional.empty();
+        return Optional.ofNullable(allPosts.get(id));
     }
 
     public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(nextId++);
+        }
+        allPosts.put(post.getId(), post);
         return post;
     }
 
     public void removeById(long id) {
+        allPosts.remove(id);
     }
 }
